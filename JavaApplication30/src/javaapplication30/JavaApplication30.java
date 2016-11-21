ec4ee5d8a40ac03634bfa4bcd7902cc947f49261
Diego <Diego@Diego>
2016-11-21 09:26:08 +0100

    Refactorizacion segundo else

--- JavaApplication30/src/javaapplication30/JavaApplication30.java
+++ JavaApplication30/src/javaapplication30/JavaApplication30.java
@@ -11,42 +11,34 @@ package javaapplication30;
  */
 public class JavaApplication30 {
 
-    public static boolean p = false;
+    public static boolean primo = false;
+
     public static void main(String[] args) {
         int numDigitos;
         int calcDigit;
-        Refac objeto=new Refac();
-        numDigitos=objeto.pedirNumDigitos();
+        Refac objeto = new Refac();
+        numDigitos = objeto.pedirNumDigitos();
         for (int i = 1; i <= 99999; i++) {
             calcDigit = objeto.calculaDigitos(i);
 
             if (calcDigit == numDigitos) {
-                if (i < 4)
-                    p = true;
-                else  {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite)
-                    {
-                        if (i % i1 == 0) contador1++;
-                            i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
+                if (i < 4) {
+                    primo = true;
+                } else if (i % 2 == 0) {
+                    primo = false;
+                } else {
+                    int metodo = objeto.siNoEsPrimo(i);
+
+                    if (metodo == 1) {
+                        primo = true;
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                }
+
+                if (primo == true) {
+                    System.out.println(i);
+                }
             }
         }
     }
-    
+
 }
